5b18ecbe205329259457e08e53b5bf5f37a8272e
Marcus Cobden <git@marcuscobden.co.uk>
2011-05-25 12:44:24 +0100

    Fixed coding style and phpdoc, added comments.

--- classes/kohana/kohana/exception.php
+++ classes/kohana/kohana/exception.php
@@ -78,7 +78,7 @@ class Kohana_Kohana_Exception extends Exception {
 	 *
 	 * @uses    Kohana_Exception::text
 	 * @param   object   exception object
-	 * @param   object   stack trace override array
+	 * @param   array    stack trace override array
 	 * @return  boolean
 	 */
 	public static function handler(Exception $e, array $trace = NULL)
@@ -92,9 +92,11 @@ class Kohana_Kohana_Exception extends Exception {
 			$file    = $e->getFile();
 			$line    = $e->getLine();
 
-			// Get the exception backtrace
-			if (! $trace)
+			if ( ! $trace)
+			{
+				// Get the exception backtrace
 				$trace = $e->getTrace();
+			}
 
 			if ($e instanceof ErrorException)
 			{
